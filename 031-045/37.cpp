6e0b39960bf3608645722f68391e43b45c33023a
Javvaji Venkata Koushik <javvajikoushik2004@gmail.com>
2026-03-23 00:01:21 +0530

    updated

---
+++ 031-045/37.cpp
@@ -0,0 +1,153 @@
+/*
+*************************************************
+*               Koushik's Template              *
+*          Om namho Venkatesaya                 *
+*************************************************
+*/
+
+#include <bits/stdc++.h>
+// #include <ext/pb_ds/assoc_container.hpp>
+// #include <ext/pb_ds/tree_policy.hpp>
+// using namespace __gnu_pbds;
+using namespace std;
+
+/************************************************
+*                 Macros                        *
+*************************************************/
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 998244353
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define int long long
+/************************************************
+*                 Typedefs                      *
+*************************************************/
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+
+// template <typename T>
+// using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+/************************************************
+*             Utility Functions                 *
+*************************************************/
+ll gcd(ll a, ll b) { return b ? gcd(b, a % b) : a; }
+ll lcm(ll a, ll b) { return (a / gcd(a, b)) * b; }
+ll mod_add(ll a, ll b, ll m) { a %= m; b %= m; return (a + b) % m; }
+ll mod_sub(ll a, ll b, ll m) { a %= m; b %= m; return (a - b + m) % m; }
+ll mod_mul(ll a, ll b, ll m) {return (a % m * (b % m)) % m;}
+ll mod_expo(ll a, ll b, ll m) { ll res = 1; while (b > 0) { if (b & 1) res = (res * a) % m; a = (a * a) % m; b >>= 1; } return res; }
+bool revsort(ll a, ll b) { return a > b; }
+vector<ll> sieve(int n) { vector<bool> is_prime(n+1, true); vector<ll> primes; for (ll i = 2; i <= n; i++) { if (is_prime[i]) { primes.push_back(i); for (ll j = i * i; j <= n; j += i) if(j<=n) is_prime[j] = false; } } return primes; }
+
+/************************************************
+*              Debugging Section                *
+*************************************************/
+#ifdef Koushik
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#elif !defined(ONLINE_JUDGE)
+#define debug(x) cerr << #x << " "; _print(x); cerr << endl;
+#else
+#define debug(x);
+#endif
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/************************************************
+*               Main Function                   *
+*************************************************/
+long long binpow(long long a, long long b) {
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a;
+        a = a * a;
+        b >>= 1;
+    }
+    return res;
+}
+vector<int>temp = sieve(1000000);
+map<int , int>mp1;
+bool valid(int x){
+    bool flag = true;
+    int val = x;
+    vector<int>vals;
+    while(val != 0){
+        vals.pb(val % 10);
+        val /= 10;
+    }
+    reverse(vals.begin() , vals.end());
+    // debug(vals);
+    int currVal = 0;
+    for(int i = 0 ;i < vals.size() ; i++){
+        currVal = currVal * 10 + vals[i];
+        // debug(currVal);
+        if(mp1[currVal] == 0){
+            flag = false;
+        }
+    }
+    int tempVal = 0;
+    currVal = 0;
+    for(int i = vals.size() - 1 ;i >= 0 ; i--){
+        currVal += (vals[i] * pow(10 , tempVal));
+        // debug(currVal);
+        if(mp1[currVal] == 0){
+            flag = false;
+        }
+        tempVal++;
+    }
+    return flag;
+}
+void solve(){
+    int res = 0;
+    for(int i = 0 ; i < temp.size() ; i++){
+        mp1[temp[i]] = 1;
+    }
+    for(int i = 10 ; i <= 1000000 ; i++){
+        int isValid = valid(i);
+        if(isValid && mp1[i]){
+            res += i;
+            debug(i);
+        }
+    }
+    cout << res << nline;
+}
+
+int32_t main() {
+    fastio();
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    freopen("error.txt", "w", stderr);
+    #endif
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
